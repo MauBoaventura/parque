a5f288220b2e65fb76fe1b42d2e55d6beed2725d
Mauricio Boaventura <mba097@gmail.com>
2020-02-05 13:01:34 -0300

    Commit inicial

---
+++ site.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser")
+
+const admin = require("./routes/admin")
+const eventos = require("./routes/eventos")
+const promo = require("./routes/promo")
+const temas = require("./routes/temas")
+
+const mongoose = require('mongoose');
+
+const path = require("path")
+
+const app = express();
+
+//Config
+//Template engine
+app.engine('handlebars', handlebars({ defaulLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//Body-Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+//public
+app.use(express.static(path.join(__dirname, "public")))
+//database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/siteparque').then(() => {
+    console.log("Conectado ao banco de dados: siteparque com sucesso")
+}).catch((erro) => {
+    console.log("Erro ao se conectar ao banco de dados: " + erro)
+})
+
+//Rotas
+app.use("/admin", admin)
+app.use("/eventos", eventos)
+app.use("/promo", promo)
+app.use("/temas", temas)
+
+app.listen(8081, function () {
+    console.log("Servidor online rodando na porta: 8081");
+});
